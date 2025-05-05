f35efe32ad2aa25f5c286ff29ba0f5766cc8f9bb
RustamKuramshin <leo.in.empty@gmail.com>
2025-05-06 00:46:13 +0300

    init commit

--- download-requirements.sh
+++ download-requirements.sh
@@ -5,7 +5,7 @@ DOC_ID="1-JOCyAptFwL30WpuQPYt4SX5FAIuRJt5nKTj6EHBL04"
 
 # Имя временного и итогового файла
 TEMP_FILE="temp_doc.txt"
-OUTPUT_FILE="google_doc_clean.txt"
+OUTPUT_FILE="project_requirements.txt"
 
 # Скачивание документа в текстовом формате
 curl -sL "https://docs.google.com/document/d/${DOC_ID}/export?format=txt" -o "${TEMP_FILE}"
@@ -17,4 +17,4 @@ grep -v '^[[:space:]]*$' "${TEMP_FILE}" > "${OUTPUT_FILE}"
 rm -f "${TEMP_FILE}"
 
 # Сообщение об успехе
-echo "Документ сохранен без пустых строк в: ${OUTPUT_FILE}"
+echo "Требования сохранены в: ${OUTPUT_FILE}"
