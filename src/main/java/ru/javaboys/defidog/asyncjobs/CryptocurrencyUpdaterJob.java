6b0f575377518d553b9ff3759d81844839fa7389
WerderRZ <rzaxel@gmail.com>
2025-05-13 01:22:34 +0300

    CMC Updater Period Run Fix

--- src/main/java/ru/javaboys/defidog/asyncjobs/CryptocurrencyUpdaterJob.java
+++ src/main/java/ru/javaboys/defidog/asyncjobs/CryptocurrencyUpdaterJob.java
@@ -1,23 +1,45 @@
 package ru.javaboys.defidog.asyncjobs;
 
 import io.jmix.core.security.SystemAuthenticator;
-import lombok.RequiredArgsConstructor;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
 import ru.javaboys.defidog.crypto.CryptocurrencyService;
 
+import java.time.Duration;
+
 @Component
-@RequiredArgsConstructor
 @ConditionalOnProperty(prefix = "coinmarketcap.updater", name = "enabled", havingValue = "true")
 public class CryptocurrencyUpdaterJob {
 
     private final CryptocurrencyService cryptocurrencyService;
     private final SystemAuthenticator systemAuthenticator;
+    private final TaskScheduler taskScheduler;
 
-    @Scheduled(fixedRate = 30 * 1000)
-    public void updateCryptocurrenciesPeriodically() {
+    @Value("${coinmarketcap.updater.interval-in-seconds}")
+    private long intervalInSeconds;
+
+    @Autowired
+    public CryptocurrencyUpdaterJob(CryptocurrencyService cryptocurrencyService,
+                                    SystemAuthenticator systemAuthenticator,
+                                    @Qualifier("core_ThreadPoolTaskScheduler") TaskScheduler taskScheduler) {
+        this.cryptocurrencyService = cryptocurrencyService;
+        this.systemAuthenticator = systemAuthenticator;
+        this.taskScheduler = taskScheduler;
+    }
+
+    @PostConstruct
+    public void scheduleTask() {
+        taskScheduler.scheduleAtFixedRate(this::updateCryptocurrenciesPeriodically, Duration.ofSeconds(intervalInSeconds));
+    }
+
+    private void updateCryptocurrenciesPeriodically() {
         systemAuthenticator.runWithSystem(cryptocurrencyService::updateCryptocurrenciesAndDefiProtocolsLogo);
         systemAuthenticator.runWithSystem(cryptocurrencyService::updateCryptocurrenciesInfo);
     }
+
 }
